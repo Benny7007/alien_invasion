e3b087b6170d467f49fc9853093d0d1e8eeb9053
sh <1104665894@qq.com>
2025-09-06 17:28:36 +0800

    添加了子弹设置

--- settings.py
+++ settings.py
@@ -8,3 +8,9 @@ class Settings:
         self.bg_color=("black")
         #飞船的设置
         self.ship_speed=1.5
+        #子弹设置
+        self.bullet_speed=3.0
+        self.bullet_width=3
+        self.bullet_height=15
+        self.bullet_color=("white")
+        self.bullets_allowed=10
