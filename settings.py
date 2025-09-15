b04eb378c896d11aa05cd9bf8b7dfb084b3b220f
sh <1104665894@qq.com>
2025-09-15 21:12:12 +0800

    添加self.ship_limit=3

--- settings.py
+++ settings.py
@@ -8,15 +8,15 @@ class Settings:
         self.bg_color=("black")
         #飞船的设置
         self.ship_speed=1.5
+        self.ship_limit=3
         #子弹设置
         self.bullet_speed=3.0
         self.bullet_width=3
         self.bullet_height=15
         self.bullet_color=("white")
         self.bullets_allowed=10
-        # self.bullets=[]
         #外星人设置
         self.alien_speed=1.0
-        self.fleet_drop_speed=10
+        self.fleet_drop_speed=8
         #fleet_dircetion为1表示向右移动，为-1表示向左移动
         self.fleet_direction=1
